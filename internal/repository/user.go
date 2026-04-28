322561bac2ba5efefe8021c56ca9f9c164a519b6
Agada Daniel <unogwuagada@gmail.com>
2026-04-28 20:48:53 +0100

    update user function added

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,11 +2,13 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/agdaniel10/Go-BasicAPI/internal/model"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type UserRepository struct {
@@ -38,6 +40,69 @@ func (r *UserRepository) FindByID(ctx context.Context, id string) (*model.User, 
 	}
 
 	return &user, nil
+}
+
+func (r *UserRepository) FindByEmail(ctx context.Context, email string) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	var user model.User
+	err := r.collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, nil
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) UpdateUser(ctx context.Context, id string, user *model.User) (*model.User, error) {
+
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	objID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	updateFields := bson.M{}
+
+	if user.Name != "" {
+		updateFields["name"] = user.Name
+	}
+
+	if user.Email != "" {
+		updateFields["email"] = user.Email
+	}
+
+	if len(updateFields) == 0 {
+		return nil, errors.New("No fields found")
+	}
+
+	filter := bson.M{"_id": objID}
+	update := bson.M{"$set": updateFields}
+
+	ops := options.FindOneAndUpdate().SetReturnDocument(options.After)
+
+	var updatedUser model.User
+	err = r.collection.FindOneAndUpdate(ctx, filter, update, ops).Decode(&updatedUser)
+
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &updatedUser, nil
 }
 
 func (r *UserRepository) FindAll(ctx context.Context) ([]model.User, error) {
--- internal/service/user.go
+++ internal/service/user.go
@@ -12,7 +12,9 @@ type UserService struct {
 }
 
 func NewUserService(repo *repository.UserRepository) *UserService {
-	return &UserService{repo: repo}
+	return &UserService{
+		repo: repo,
+	}
 }
 
 func (s *UserService) GetAll(ctx context.Context) ([]model.User, error) {
@@ -23,6 +25,14 @@ func (s *UserService) GetByID(ctx context.Context, id string) (*model.User, erro
 	return s.repo.FindByID(ctx, id)
 }
 
+func (s *UserService) GetByEmail(ctx context.Context, email string) (*model.User, error) {
+	return s.repo.FindByEmail(ctx, email)
+}
+
 func (s *UserService) Create(ctx context.Context, user *model.User) error {
 	return s.repo.Create(ctx, user)
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, id string, user *model.User) (*model.User, error) {
+	return s.repo.UpdateUser(ctx, id, user)
 }
